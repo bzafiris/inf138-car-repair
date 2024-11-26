ea1264caa7f13b98051b1b77d7f71969c9cce354
Bill Zafeiris <bzafiris@gmail.com>
2024-11-26 10:55:03 +0200

    Project setup, first domain classes

---
+++ app/src/main/java/com/example/repair/domain/Appointment.java
@@ -0,0 +1,52 @@
+package com.example.repair.domain;
+
+import java.time.LocalDate;
+
+public class Appointment {
+
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private LocalDate date;
+    private Time time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+}
---
+++ app/src/main/java/com/example/repair/domain/Time.java
@@ -0,0 +1,14 @@
+package com.example.repair.domain;
+
+public class Time {
+
+    public final int hours;
+    public final int minutes;
+
+    public Time(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+
+}
